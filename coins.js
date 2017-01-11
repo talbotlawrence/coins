169baaa6bb7f87883af15d83f0ac71a27630b948
Talbot <talbotlawrence75@gmail.com>
2017-01-11 16:01:43 -0600

    updated coins.js

--- coins.js
+++ coins.js
@@ -2,7 +2,20 @@
 // Use the common American coins of quarters, dimes, nickels, and pennies.
 
 // 1. Grab the input from the user and save it to variable.
-// 2. 
+// 2. Subtract .25 from the amount.
+//		a. if the amount is still greater than .25, then do it again.  
+//			keep track of the number of times it loops on .25.
+//		b. if the amount is still greater than .25, then move on.
+// 3. Subtract .10 from the amount.
+//		a. if the amount is still greater than .10, then do it again.  
+//			keep track of the number of times it loops on .10.
+//		b. if the amount is still greater than .10, then move on.
+// 4. Subtract .05 from the amount.
+//		a. if the amount is still greater than .05, then do it again.  
+//			keep track of the number of times it loops on .05.
+//		b. if the amount is still greater than .05, then move on.
+// 5. Subtract .01 from the amount and keep track of the number of times it loops on .01.
+// 6. Display the results.
 
 /*
   Input: 0.67
