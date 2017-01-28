45f2d706a7cceab509c8b312a140c798213c00c3
Talbot <talbotlawrence75@gmail.com>
2017-01-28 12:25:39 -0600

    finished coin exercise

--- coins.js
+++ coins.js
@@ -1,22 +1,3 @@
-// Write a program that will convert a dollar amount to the number of coins that make up the amount. 
-// Use the common American coins of quarters, dimes, nickels, and pennies.
-
-// 1. Grab the input from the user and save it to variable.
-// 2. Subtract .25 from the amount.
-//		a. if the amount is still greater than .25, then do it again.  
-//			keep track of the number of times it loops on .25.
-//		b. if the amount is still greater than .25, then move on.
-// 3. Subtract .10 from the amount.
-//		a. if the amount is still greater than .10, then do it again.  
-//			keep track of the number of times it loops on .10.
-//		b. if the amount is still greater than .10, then move on.
-// 4. Subtract .05 from the amount.
-//		a. if the amount is still greater than .05, then do it again.  
-//			keep track of the number of times it loops on .05.
-//		b. if the amount is still greater than .05, then move on.
-// 5. Subtract .01 from the amount and keep track of the number of times it loops on .01.
-// 6. Display the results.
-
 /*
   Input: 0.67
   Output:
@@ -29,17 +10,57 @@
 
 */
 
-var userInput = prompt("Please enter a dollar ammount?");
-console.log("userInput", userInput);
-////////////////////////////////////////////////////////////////////////////////////
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+var button = document.getElementById("submit");
+button.addEventListener("click", coinCounter);
 
-  coinPurse.quarters = 0;
+function coinCounter () {
+
+  var userInput = document.getElementById("input").value;
+  userInput = parseFloat(userInput);
+  console.log("User input is: ", userInput);
+  
+  var quarter = .25;                              //console.log("quarter", quarter);
+  var dime = .1;
+  var nickel = .05;
+  var penny = .01;
+
+  var numberOfQuarters;
+  var numberOfDimes;
+  var numberOfNickels;
+  var numberOfPennies;
+
+  //count quarters
+  numberOfQuarters = userInput / quarter;         //console.log("numberOfQuarters", numberOfQuarters);
+  numberOfQuarters = Math.floor(numberOfQuarters);  //console.log("numberOfQuarters", numberOfQuarters);
+  var remainder = userInput - (quarter * numberOfQuarters); //console.log("remainder", remainder);
+  remainder = remainder.toFixed(2);                         //console.log("remainder", remainder);
+
+  //count dimes
+  numberOfDimes = remainder / dime;
+  numberOfDimes = Math.floor(numberOfDimes);
+  remainder = remainder - (dime * numberOfDimes);
+
+  //count nickels
+  numberOfNickels = remainder / nickel;
+  numberOfNickels = Math.floor(numberOfNickels);
+  remainder = remainder - (nickel * numberOfNickels);
+
+  //count pennies
+  numberOfPennies = remainder / penny;
+  numberOfPennies = Math.floor(numberOfPennies);
+  remainder = remainder - (penny * numberOfPennies);
+
+  var coinPurse = {
+    quarters: numberOfQuarters,
+    dimes: numberOfDimes,
+    nickels: numberOfNickels,
+    pennies: numberOfPennies
+  };
+
+  console.log(coinPurse);
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+// var coins = coinCounter();
+// console.log(coins);
